888c77eebdab09b4eda66a0650e613caf25b062e
kohekondo <1211lta@gmail.com>
2017-11-21 12:40:32 +0900

    uiはとりあえず作成

--- MyTodoList/ViewController.swift
+++ MyTodoList/ViewController.swift
@@ -8,16 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//UITableViewDataSource,UITableViewDelegateのプロトコルを実装する旨の宣言を行う
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
